0845779b11e8caee791c70e8c842d32c65178447
Jonathan van der Riet <23583290@sun.ac.za>
2026-01-27 13:19:32 +0000

    feat: add HIL Node support and update build configurations

--- apps/hil-node/src/main.c
+++ apps/hil-node/src/main.c
@@ -1,10 +1,17 @@
+// main.c
 #include <stdio.h>
 #include "pico/stdlib.h"
 
 int main() {
+    // Initialize USB Stdstdio
     stdio_init_all();
+    
+
+
     while (true) {
-        printf("Hello from Sensor Node!\n");
+        printf("Hello from Minimal HIL Node!\n");
         sleep_ms(1000);
     }
 }
+
+
